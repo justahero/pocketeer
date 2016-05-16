d2054a5a2c3beea133f05f2cabe67e76a24177e2
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2016-05-16 17:47:51 +0200

    Refactors get methods to get extra options parameter

--- lib/pocketeer/get.ex
+++ lib/pocketeer/get.ex
@@ -3,29 +3,30 @@ defmodule Pocketeer.Get do
 
   alias Pocketeer.HTTPHandler
 
-  def get(client) do
-    HTTPotion.post("#{client.site}/v3/get", default_args(client))
+  def get(client, options \\ %{}) do
+    HTTPotion.post("#{client.site}/v3/get", default_args(client, options))
     |> handle_response
   end
 
-  def get!(client) do
-    response = HTTPotion.post("#{client.site}/v3/get", default_args(client)) |> handle_response
+  def get!(client, options \\ %{}) do
+    response = HTTPotion.post("#{client.site}/v3/get", default_args(client, options))
+    |> handle_response
     case response do
       {:ok, body}      -> body
       {:error, reason} -> raise reason
     end
   end
 
-  defp default_options(client) do
+  defp default_options(client, options) do
     %{
       consumer_key: client.consumer_key,
       access_token: client.access_token
     } |> Poison.encode!
   end
 
-  defp default_args(client) do
+  defp default_args(client, options) do
     [
-      body: default_options(client),
+      body: default_options(client, options),
       headers: request_headers
     ]
   end
