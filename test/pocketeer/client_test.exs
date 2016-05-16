7db12f5604c548101b5702e93917942d74db8ade
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2016-05-16 11:58:34 +0200

    Adds test helper to load json fixtures from file
    * adds sample json file to fixtures folder

--- test/pocketeer/client_test.exs
+++ test/pocketeer/client_test.exs
@@ -1,4 +1,10 @@
 defmodule Pocketeer.ClientTest do
   use ExUnit.Case
+  import PathHelpers
+
   doctest Pocketeer.Client
+
+  test "GET" do
+    {:ok, json} = load_json("retrieve_sample.json")
+  end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,19 @@
 ExUnit.start
 Application.ensure_all_started(:bypass)
+
+defmodule PathHelpers do
+  def fixture_path do
+    Path.expand("fixtures", __DIR__)
+  end
+
+  def fixture_path(file_path) do
+    Path.join fixture_path, file_path
+  end
+
+  def load_json(file_path) do
+    case file_path |> fixture_path |> File.read do
+      {:ok, file}      -> {:ok, Poison.decode!(file)}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+end
