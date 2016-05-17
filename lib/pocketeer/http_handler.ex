6f4b0def008d50400e6b47bb974c63679767dd12
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2016-05-17 12:57:01 +0200

    Sets user agent for lib

--- lib/pocketeer/http_handler.ex
+++ lib/pocketeer/http_handler.ex
@@ -4,7 +4,8 @@ defmodule Pocketeer.HTTPHandler do
 
   @request_headers [
     {"Content-Type", "application/json; charset=UTF-8"},
-    {"X-Accept", "application/json"}
+    {"X-Accept", "application/json"},
+    {"User-Agent", "Pocketeer"}
   ]
 
   def request_headers do
