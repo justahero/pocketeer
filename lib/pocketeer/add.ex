bfc92c430cd809393e5976da891c5d9336b7e37e
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2016-05-19 00:10:26 +0200

    Refactors Add functions using some overloads
    * fixes all tests

--- lib/pocketeer/add.ex
+++ lib/pocketeer/add.ex
@@ -23,28 +23,25 @@ defmodule Pocketeer.Add do
             tags: [],
             tweet_id: nil
 
-  @spec new(Keyword.t) :: t
+  @spec new(map) :: t
+  def new(%{url: _, tags: _} = options) do
+    options = %{options | url: URI.encode_www_form(options[:url])}
+    options = %{options | tags: Enum.join(options[:tags], ", ")}
+    struct(__MODULE__, options)
+  end
   def new(%{url: _} = options) do
+    options = %{options | url: URI.encode_www_form(options[:url])}
     struct(__MODULE__, options)
   end
 
-  def add(client, %Add{url: _} = options) do
-    add(client, Add.new(options))
-  end
-
-  def url(options \\ %{}, uri) do
-    Map.put(options, :url, uri)
-  end
-
-  @spec tags(map, list) :: map
-  def tags(options \\ %{}, tags \\ []) do
-    Map.put(options, :tags, tags)
-  end
-
   @spec add(Client.t, Add.t) :: {:ok, Response.t} | {:error, HTTPError.t}
-  def add(client, %{url: _} = options) do
-    options = %{options | url: URI.encode_www_form(options[:url])}
+  def add(client, options = %Add{url: _}) do
     HTTPotion.post("#{client.site}/v3/add", default_args(client, options))
     |> handle_response
+  end
+
+  @spec add(Client.t, map) :: {:ok, Response.t} | {:error, HTTPError.t}
+  def add(client, options = %{url: _}) do
+    add(client, Add.new(options))
   end
 end
--- test/pocketeer/add_test.exs
+++ test/pocketeer/add_test.exs
@@ -12,37 +12,32 @@ defmodule Pocketeer.AddTest do
     {:ok, server: server, client: client}
   end
 
-  test "creates options via Add" do
-    options = Add.url("http://example.com") |> Add.tags(["foo", "bar"])
-    assert options == %{url: "http://example.com", tags: ["foo", "bar"]}
-  end
-
   test "add with url only", %{server: server, client: client} do
     bypass server, "POST", "/v3/add", fn conn ->
       assert conn.query_string == ""
       assert conn.body_params["access_token"] == "1234"
       assert conn.body_params["consumer_key"] == "abcd"
-      assert conn.body_params["url"] == URI.encode_www_form("http://localhost/me")
-      assert Enum.count(conn.body_params) == 4
+      assert conn.body_params["url"] == URI.encode_www_form("http://example.com")
       json_response(conn, 200, "add_success.json")
     end
 
-    options = %{url: "http://localhost/me"}
+    options = %{url: "http://example.com"}
     {:ok, body} = Pocketeer.Add.add(client, options)
     assert Poison.Parser.parse!(body)
   end
 
-  test "add with struct", %{server: server, client: client} do
+  test "add with parameters", %{server: server, client: client} do
     bypass server, "POST", "/v3/add", fn conn ->
       assert conn.query_string == ""
       assert conn.body_params["url"] == URI.encode_www_form("http://example.com")
-      # assert conn.body_params["tags"] == "foo, bar"
-      # assert conn.body_params["tweet_id"] == "tweet"
-      # assert conn.body_params["title"] == "Try it"
+      assert conn.body_params["tags"] == "foo, bar"
+      assert conn.body_params["tweet_id"] == "tweet"
+      assert conn.body_params["title"] == "Try it"
       json_response(conn, 200, "add_success.json")
     end
 
-    options = Add.url("http://example.com")
+    options = %{url: "http://example.com", title: "Try it", tweet_id: "tweet", tags: ["foo", "bar"]}
     {:ok, body} = Pocketeer.Add.add(client, options)
+    assert Poison.Parser.parse!(body)
   end
 end
