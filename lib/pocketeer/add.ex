719b239123fc18f5a58b51b98b1f5538ee416948
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2016-05-20 00:12:06 +0200

    Adds documentation

--- lib/pocketeer/add.ex
+++ lib/pocketeer/add.ex
@@ -4,10 +4,23 @@ defmodule Pocketeer.Add do
   """
 
   import Pocketeer.HTTPHandler
-  alias Pocketeer.Add
 
+  @doc """
+  Adds / Creates a new item on Pocket
+
+  ## Parameters
+
+  `client`: A `Pocketeer.Client` struct that contains `consumer_key` and `access_token`
+  `options`: An options map
+
+    - `url`: The url to save in Pocket (required)
+    - `tags`: A list of tags (optional)
+    - `title`: The title of the entry (optional)
+    - `tweet_id`: The id of the tweet to link the item with (optional)
+
+  """
   @spec add(Client.t, map) :: {:ok, Response.t} | {:error, HTTPError.t}
-  def add(client, %{url: url} = options) do
+  def add(client, %{url: _} = options) do
     args = default_args(client, options |> parse_options)
     HTTPotion.post("#{client.site}/v3/add", args)
     |> handle_response
