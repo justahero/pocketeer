d6756de15a418acb80a34e79fec1b9a696efbe3b
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2016-05-21 21:32:23 +0200

    Fixes and refactors assertions for modifying items

--- test/pocketeer/send_test.exs
+++ test/pocketeer/send_test.exs
@@ -17,29 +17,41 @@ defmodule Pocketeer.SendTest do
       assert conn.query_string == ""
       assert conn.body_params["access_token"] == "1234"
       assert conn.body_params["consumer_key"] == "abcd"
-      assert_map %{"action": "archive", "item_id": "1234"}, conn.body_params["actions"]
+      assert_include %{"action": "archive", "item_id": "1234"}, conn.body_params["actions"]
       json_response(conn, 200, "send_favorite_success.json")
     end
 
     Send.post(Send.archive("1234"), client)
   end
 
-  test "send several actions", %{server: server, client: client} do
+  test "archive multiple items", %{server: server, client: client} do
     bypass server, "POST", "/v3/send", fn conn ->
-      assert_map %{"action": "archive", "item_id": "1234"}, conn.body_params["actions"]
+      assert_include %{"action": "archive", "item_id": "1234"}, conn.body_params["actions"]
+      assert_include %{"action": "archive", "item_id": "9876"}, conn.body_params["actions"]
       json_response(conn, 200, "send_favorite_success.json")
     end
 
-    # Send.send(client, Send.archive(["1234", "9876"]))
     Send.new
     |> Send.archive(["1234", "9876"])
     |> Send.post(client)
   end
 
-  def assert_map(expected, actual) do
-    actual
-    |> List.first
-    |> Map.drop(["timestamp"])
-    |> Map.equal?(expected)
+  test "unarchive item", %{server: server, client: client} do
+    bypass server, "POST", "/v3/send", fn conn ->
+      assert_include %{"action": "archive", "item_id": "1234"}, conn.body_params["actions"]
+      assert_include %{"action": "archive", "item_id": "9876"}, conn.body_params["actions"]
+      json_response(conn, 200, "send_favorite_success.json")
+    end
+
+    Send.new
+    |> Send.unarchive("1234")
+    |> Send.unarchive("9876")
+    |> Send.post(client)
+  end
+
+  def assert_include(expected, actual) when is_list(actual) do
+    Enum.any? actual, fn item ->
+      Map.drop(item, ["timestamp"]) |> Map.equal?(expected)
+    end
   end
 end
