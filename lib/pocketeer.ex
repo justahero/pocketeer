6b2e8acde7e6920deca6b0015ee0d2bb99e87b97
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2016-05-25 21:00:13 +0200

    Adds documentation to modules and functions

--- lib/pocketeer.ex
+++ lib/pocketeer.ex
@@ -1,6 +1,8 @@
 defmodule Pocketeer do
   import Pocketeer.HTTPHandler
 
+  alias Pocketeer.Client
+
   @doc """
   Main function to retrieve articles / items from the Pocket Retrieve API
 
@@ -10,7 +12,6 @@ defmodule Pocketeer do
     - `options`: The options struct for the Retrieve API endpoint
 
   TODO describe all options
-
   """
   @spec get(Client.t, Get.t) :: {:ok, Response.t} | {:error, HTTPError.t}
   def get(%Client{} = client, options \\ %{}) do
@@ -18,7 +19,18 @@ defmodule Pocketeer do
     |> handle_response
   end
 
-  def get!(client, options \\ %{}) do
+  @doc """
+  Main function to retrieve articles / items from the Pocket Retrieve API
+
+  ## Parameters
+
+    - `client`: The API client with consumer key and access token
+    - `options`: The options struct for the Retrieve API endpoint
+
+  TODO describe all options
+  """
+  @spec get!(Client.t, Get.t) :: {:ok, Response.t} | {:error, HTTPError.t}
+  def get!(%Client{} = client, options \\ %{}) do
     case get(client, options) do
       {:ok, body}      -> body
       {:error, reason} -> raise reason
--- lib/pocketeer/auth.ex
+++ lib/pocketeer/auth.ex
@@ -1,13 +1,24 @@
 defmodule Pocketeer.Auth do
+  @moduledoc """
+  Authorization module to fetch `request_token` and `access_token` from the Pocket API
+  necessary to retrieve, modify or access items.
+  """
+
   import Pocketeer.HTTPHandler
+  alias Pocketeer.Response
 
   @doc """
-  Returns the URL to authorize the application in the Pocket API
+  Returns authorization URL for Pocket to authorize the application.
+
+  ## Examples
+
+      iex> Pocketeer.Auth.authorize_url("abcd", "http://example")
+      "https://getpocket.com/auth/authorize?request_token=abcd&redirect_uri=http%3A%2F%2Fexample.com"
 
   ## Parameters
 
-    - request_token: The request token to authorize the app with
-    - redirect_uri: The URL the user is redirected to after authorization
+    - `request_token`: The request token to authorize the app with
+    - `redirect_uri`: The URL the user is redirected to after authorization
 
   Returns the authorization URL with all parameters
   """
@@ -17,23 +28,57 @@ defmodule Pocketeer.Auth do
   end
 
   @doc """
-  Sends a request to fetch a request token
+  Returns authorization URL for Pocket to authorize the application.
+
+  ## Examples
+
+      iex> Pocketeer.Auth.authorize_url(%{request_token: "abcd", redirect_uri: "http://example.com"})
+      "https://getpocket.com/auth/authorize?request_token=abcd&redirect_uri=http%3A%2F%2Fexample.com"
+
+  ## Parameters
+
+    - A struct with `request_token` and `redirect_uri` present.
+
+  """
+  @spec authorize_url(map) :: String.t
+  def authorize_url(%{request_token: token, redirect_uri: uri}) do
+    authorize_url(token, uri)
+  end
+
+  @doc """
+  Sends a request to fetch a `request_token` from Pocket.
+
+  ## Examples
+
+  ```
+  Pocketeer.Auth.get_request_token("1234", "http://example.com")
+  ```
 
   ## Parameters:
 
-    - consumer_key: The consumer key used from the Pocket API
-    - redirect_uri: URL to redirect when the authorization process has been completed
+    - `consumer_key`: The consumer key used from the Pocket API
+    - `redirect_uri`: URL to redirect when the authorization process has been completed
 
-  Returns `HTTP.Response` if request succeeds
-  Returns `Pocketeer.HTTPError` if a request fails
   """
-  @spec get_request_token(String.t, String.t) :: {:ok, map} | {:error, HTTPError.t}
+  @spec get_request_token(String.t, String.t) :: {:ok, Response.t} | {:error, HTTPError.t}
   def get_request_token(consumer_key, redirect_uri) do
     body = ~s({"consumer_key": "#{consumer_key}", "redirect_uri": "#{redirect_uri}"})
     HTTPotion.post(request_token_url, [body: body, headers: request_headers])
     |> handle_response
   end
-  @spec get_request_token(map) :: {:ok, map} | {:error, HTTPError.t}
+
+  @doc """
+  Sends a request to fetch a `request_token` from Pocket.
+  
+  ## Examples
+
+  ```
+  options = %{consumer_key: "1234", redirect_uri: "http://example.com"}
+  Pocketeer.Auth.get_request_token(options)
+  ```
+
+  """
+  @spec get_request_token(map) :: {:ok, Response.t} | {:error, HTTPError.t}
   def get_request_token(%{consumer_key: key, redirect_uri: uri}) do
     get_request_token(key, uri)
   end
@@ -41,23 +86,44 @@ defmodule Pocketeer.Auth do
   @doc """
   Requests an access token from the Pocket API.
 
-  The method requires the `request_token` fetched from `Pocketeer.Auth.get_request_token`
+  The method requires a `request_token` fetched from `Pocketeer.Auth.get_request_token`
   method.
+
+  ## Examples
+
+  ```
+  Pocketeer.Auth.get_access_token("abcd", "1234")
+  {:ok, Response{}}
+  ```
 
   ## Parameters
 
-    - consumer_key: The consumer key used from the Pocket API
-    - request_token: The request token to authorize the application
+    - `consumer_key`: The consumer key used from the Pocket API
+    - `request_token`: The request token to authorize the application
 
-  Returns `HTTP.Response` if request succeeds
-  Returns `Pocketeer.HTTPError` if a request fails
   """
-  @spec get_access_token(String.t, String.t) :: {:ok, map} | {:error, HTTPError.t}
+  @spec get_access_token(String.t, String.t) :: {:ok, Response.t} | {:error, HTTPError.t}
   def get_access_token(consumer_key, request_token) do
     body = ~s({"consumer_key": "#{consumer_key}", "code": "#{request_token}"})
     HTTPotion.post(access_token_url, [body: body, headers: request_headers])
     |> handle_response
   end
+
+  @doc """
+  Requests an access token from the Pocket API.
+
+  The method requires a `request_token` fetched from `Pocketeer.Auth.get_request_token`
+  method.
+
+  ## Examples
+
+  ```
+  Pocketeer.Auth.get_access_token(%{consumer_key: "abcd", request_token: "1234"})
+  {:ok, Response{}}
+  ```
+
+  """
+  @spec get_access_token(map) :: {:ok, map} | {:error, HTTPError.t}
   def get_access_token(%{consumer_key: key, request_token: token}) do
     get_access_token(key, token)
   end
