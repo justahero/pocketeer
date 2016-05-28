420f935c368f6ff7379d4d5f18e3fce1c4d9e220
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2016-05-28 10:52:02 +0200

    Moves add functionas into Pocketeer base module
    This makes the API a bit cleaner and groups all request methods into a single place.

--- lib/pocketeer.ex
+++ lib/pocketeer.ex
@@ -1,11 +1,14 @@
 defmodule Pocketeer do
   @moduledoc """
-  Implements functions for sending requests to the Pocket API.
+  Functions to send requests to the Pocket API.
   """
 
   import Pocketeer.HTTPHandler
+  import Pocketeer.TagsHelper
 
   alias Pocketeer.Client
+
+  @add_options [:url, :tags, :title, :tweet_id]
 
   @doc """
   Fetches a list of articles / items from the Pocket API.
@@ -72,4 +75,54 @@ defmodule Pocketeer do
       {:error, reason} -> raise reason
     end
   end
+
+  @doc """
+  Saves a new article on Pocket.
+
+  ## Parameters
+
+  `client` - either a `Pocketeer.Client` struct or a map that contains `consumer_key` and `access_token`.
+
+  `options` - an options map with properties to save
+
+    * `url` - The url to save in Pocket (required)
+    * `tags` - A list of tags (optional)
+    * `title` - The title of the entry (optional)
+    * `tweet_id` - The id of the tweet to link the item with (optional)
+
+  ## Examples
+
+  To store an item with url, title and tags with a `Pocketeer.Client`.
+
+  ```
+  client = Pocketeer.Client.new("consumer_key", "access_token")
+  Pocketeer.add(client, %{url: "http://example.com", title: "Hello", tags: "news"})
+  ```
+
+  For storing an article with reference to a tweet.
+
+  ```
+  options = %{url: "http://linkto.me", tweet_id: "5678"}
+  Pocketeer.add(%{consumer_key: "abcd", access_token: "1234"}, options)
+  ```
+
+  """
+  @spec add(Client.t | map, map) :: {:ok, Response.t} | {:error, HTTPError.t}
+  def add(%Client{} = client, %{url: _} = options) do
+    {options, _} = Dict.split(options, @add_options)
+    args = default_args(client, options |> parse_options)
+    HTTPotion.post("#{client.site}/v3/add", args)
+    |> handle_response
+  end
+  def add(%{consumer_key: key, access_token: token}, %{url: _} = options) do
+    add(Client.new(key, token), options)
+  end
+
+  # private methods
+
+  defp parse_options(%{tags: tags} = options) do
+    %{options | tags: parse_tags(tags)}
+  end
+  defp parse_options(%{} = options) do options end
+
 end
--- test/pocketeer/pocketeer_test.exs
+++ test/pocketeer/pocketeer_test.exs
@@ -59,4 +59,42 @@ defmodule PocketeerTest do
     {:ok, body} = Pocketeer.get(client, options)
     assert Poison.Parser.parse!(body)
   end
+
+  test "add with url only", %{server: server, client: client} do
+    bypass server, "POST", "/v3/add", fn conn ->
+      assert conn.query_string == ""
+      assert conn.body_params["access_token"] == "1234"
+      assert conn.body_params["consumer_key"] == "abcd"
+      assert conn.body_params["url"] == "http://example.com"
+      json_response(conn, 200, "add_success.json")
+    end
+
+    options = %{url: "http://example.com"}
+    {:ok, _body} = Pocketeer.add(client, options)
+  end
+
+  test "add with parameters", %{server: server, client: client} do
+    bypass server, "POST", "/v3/add", fn conn ->
+      assert conn.query_string == ""
+      assert conn.body_params["url"] == "http://example.com"
+      assert conn.body_params["tags"] == "foo, bar"
+      assert conn.body_params["tweet_id"] == "tweet"
+      assert conn.body_params["title"] == "Try it"
+      json_response(conn, 200, "add_success.json")
+    end
+
+    options = %{url: "http://example.com", title: "Try it", tweet_id: "tweet", tags: ["foo", "bar"]}
+    {:ok, _body} = Pocketeer.add(client, options)
+  end
+
+  test "add with some ignored parameters", %{server: server, client: client} do
+    bypass server, "POST", "/v3/add", fn conn ->
+      assert conn.body_params["url"] == "http://example.com"
+      assert Map.has_key?(conn.body_params, "foo") == false
+      json_response(conn, 200, "add_success.json")
+    end
+
+    options = %{url: "http://example.com", foo: "bar"}
+    {:ok, _body} = Pocketeer.add(client, options)
+  end
 end
